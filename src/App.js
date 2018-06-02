dd20837d56e4c41d5f39e4943602db4870db65f8
John Tucker <john.tucker@sharpspring.com>
2018-06-02 11:03:07 -0400

    Query

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Counter from './components/Counter';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Counter />
       </div>
     );
   }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import gql from 'graphql-tag';
+import { PropTypes } from 'prop-types';
+import React from 'react';
+import { Query } from 'react-apollo';
+
+const CounterView = ({ counter }) => (
+  <div>{counter}</div>
+);
+
+CounterView.propTypes = {
+  counter: PropTypes.number.isRequired,
+};
+
+const GET_COUNTER = gql`
+  {
+    counter @client
+  }
+`;
+
+const Counter = () => (
+  <Query query={GET_COUNTER}>
+    {({ data }) => <CounterView {...data} />}
+  </Query>
+);
+
+export default Counter;
