c0ad77ace7d2ce08d0266b3269b57559392ab5b2
John Tucker <john.tucker@sharpspring.com>
2018-06-02 13:12:19 -0400

    Direct writes

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,12 +3,18 @@ import { PropTypes } from 'prop-types';
 import React from 'react';
 import { Query } from 'react-apollo';
 
-const CounterView = ({ counter }) => (
-  <div>{counter}</div>
+const CounterView = ({ counter, onDecrement, onIncrement }) => (
+  <div>
+    <div>{counter}</div>
+    <button onClick={onDecrement}>-</button>
+    <button onClick={onIncrement}>+</button>
+  </div>
 );
 
 CounterView.propTypes = {
   counter: PropTypes.number.isRequired,
+  onDecrement: PropTypes.func.isRequired,
+  onIncrement: PropTypes.func.isRequired,
 };
 
 const GET_COUNTER = gql`
@@ -19,7 +25,18 @@ const GET_COUNTER = gql`
 
 const Counter = () => (
   <Query query={GET_COUNTER}>
-    {({ data }) => <CounterView {...data} />}
+    {({ data, client }) => {
+      const { counter } = data;
+      const handleIncrement = () => client.writeData({ data: { counter: counter + 1 }});
+      const handleDecrement = () => client.writeData({ data: { counter: counter - 1 }});
+      return (
+       <CounterView
+          {...data}
+          onDecrement={handleDecrement}
+          onIncrement={handleIncrement}
+        />
+      );
+    }}
   </Query>
 );
 
